e9c1c46ef5cba00d82ff865742c68ff5079f860e
krithigau <krithigau2005@gmail.com>
2024-04-28 15:47:28 +0530

    Success

---
+++ mycal/calcapp/static/index.js
@@ -0,0 +1,24 @@
+function dis(val) 
+{
+    document.getElementById("result").value += val;
+}
+
+function clr() 
+{
+    document.getElementById("result").value = "";
+}
+
+function solve() 
+{
+    x = document.getElementById("result").value;
+    y = eval(x);
+    document.getElementById("result").value = y;
+}
+
+function percent() 
+{
+    x = document.getElementById("result").value;
+    y = eval(x);
+    percentage = y / 100;
+    document.getElementById("result").value = percentage;
+}
